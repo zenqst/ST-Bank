6ca20bac016b354e66cf70e6d20e9603687f09e5
zenqst <zenqst@gmail.com>
2025-07-22 19:18:46 +0300

    ci(config): :construction_worker: upd requirements.txt, version of bot
    Обновлён файл requirements.txt, обновлена версия бота 0.9 -> 0.10, обновлён README.md

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Telegram-бот с механикой трейдинга в реальном в�
 | Работа с сервером | Flask |
 | Типизация | pydantic |
 
+### pipreqs
+`pipreqs . --force --encoding utf-8`
 
 ### Протестировать бота
 [Telegram](https://t.me/ST_Bank_bot)
--- config_reader.py
+++ config_reader.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     )
     
 class Config():
-    version = 'v0.9',
+    version = 'v0.10',
     admin_id = 980316238
     rarities = {
         "legendary": {'name': 'Легендарная', 'icon': '🟡', 'chance': 1, 'compensation': 1100, 'order': 0},
